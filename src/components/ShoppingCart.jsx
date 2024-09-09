13e77d2ea811c2343fdae71ff6fff42d0e87a9f5
unknown <ngoumkwa.kenne@gmail.com>
2024-09-09 21:42:05 +0100

    update ShoppingCart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -11,17 +11,17 @@ const ShoppingCart = () => {
     <div className="  w-full  sm:w-3/4 bg-white px-10 py-10">
       <div className="flex justify-between border-b pb-8">
         <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-        <h2 className="font-semibold text-2xl">3 Items</h2>
+        <h2 className="font-semibold text-2xl">{cart.length} Articles</h2>
       </div>  
       {cart.map((item)=>(
-        <div>
+        <div key={item.id}>
         <div className="md:flex items-strech py-8 md:py-10 lg:py-8 border-t border-gray-50">
         <div className="md:w-4/12 2xl:w-1/4 w-full">
           <img src={item.image} alt="Black Leather Purse" className="h-full object-center object-cover md:block hidden" />
         </div>
         <div className="md:pl-3 md:w-8/12 2xl:w-3/4 flex flex-col justify-center">
           <div className="flex items-center justify-between w-full">
-            <p className="text-base font-black leading-none text-gray-800">title</p>
+            <p className="text-base font-black leading-none text-gray-800">{item.name}</p>
            <div>
   <label htmlFor="Quantity" className="sr-only"> Quantity </label>
 
@@ -47,9 +47,11 @@ const ShoppingCart = () => {
           <p className="w-96 text-xs leading-3 text-gray-600">Marque : Nike</p>
           <div className="flex items-center justify-between pt-5">
             <div className="flex itemms-center">
-              <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer">Remove</p>
+              <p className="text-xs leading-3 underline text-red-500 pl-5 cursor-pointer"
+              onClick={()=>deleteCart(item.id)}>
+              Remove</p>
             </div>
-            <p className="text-base font-black leading-none text-gray-800">,000</p>
+            <p className="text-base font-black leading-none text-gray-800">{item.price}</p>
           </div>
         </div>
         </div>
@@ -57,18 +59,18 @@ const ShoppingCart = () => {
       )) }
        
      
-      <a href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
+      <Link to={"/"} className="flex font-semibold text-indigo-600 text-sm mt-10">
         <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512">
           <path
             d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
         </svg>
         Continue Shopping
-      </a>
+      </Link>
     </div>
     <div id="summary" className=" w-full   sm:w-1/4   md:w-1/2     px-8 py-10">
       <h1 className="font-semibold text-2xl border-b pb-8">Order Summary</h1>
       <div className="flex justify-between mt-10 mb-5">
-        <span className="font-semibold text-sm uppercase">Items 3</span>
+        <span className="font-semibold text-sm uppercase">Items {cart.length}</span>
         <span className="font-semibold text-sm">590$</span>
       </div>
       <div>
@@ -76,7 +78,7 @@ const ShoppingCart = () => {
               Shipping
             </label>
         <select className="block p-2 text-gray-600 w-full text-sm">
-              <option>Standard shipping - $10.00</option>
+              <option>Standard shipping - 1000.00 FCFA</option>
             </select>
       </div>
       <div className="py-10">
