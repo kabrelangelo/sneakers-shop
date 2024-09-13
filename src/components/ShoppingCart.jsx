1c765318ae91c5a78284abfdac8c69d643aecd7c
unknown <ngoumkwa.kenne@gmail.com>
2024-09-13 10:07:30 +0100

    update ProductCard

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -13,7 +13,7 @@ const ShoppingCart = () => {
     <div className="  w-full  sm:w-3/4 bg-white px-10 py-10">
       <div className="flex justify-between border-b pb-8">
         <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-        <h2 className="font-semibold text-2xl">{cart.length} Articles</h2>
+       {/*  <h2 className="font-semibold text-2xl">{cart.length} Articles</h2> */}
       </div>  
       {cart.map((item)=>(
         <div key={item.product.id}>
--- src/pages/product/ProductCard.jsx
+++ src/pages/product/ProductCard.jsx
@@ -27,12 +27,12 @@ const ProductCard = () => {
         {/* <span className="text-sm text-slate-900 line-through">$699</span> */}
       </p>
     </div>
-    <Link to="/product/cart" onClick={()=>addCart(item,1)} className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+   {/*  <Link to="/cart" onClick={()=>addCart(item,1)} className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
       <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
         <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
       </svg>
       Add to cart</Link
-    >
+    > */}
   </div>
 </div>
        ))
